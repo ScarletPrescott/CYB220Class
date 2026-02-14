f8e420bedbe20c160fdf471d415277fecef9c126
ScarletPrescott <scarletelaineprescott@gmail.com>
2026-02-13 20:03:26 -0500

    user.py

---
+++ Module5/user.py
@@ -0,0 +1,47 @@
+#ScarletPrescott
+class User():
+    """Simple user profile"""
+    def __init__(self, first_name, last_name, age, username, favorite_color):
+        self.f_name = first_name
+        self.l_name = last_name
+        self.age = age
+        self.username = username
+        self.favorite_color = favorite_color
+        self.login_attempts = 0
+
+    def describe_user(self):
+        print(f"The User Details are Below:")
+        print("First Name:", self.f_name)
+        print("Last Name:", self.l_name)
+        print("Age:", self.age)
+        print("Username:", self.username)
+        print("Favorite Color:", self.favorite_color)
+
+    def greet_user(self):
+        print(f"Hello, {self.username.title()}!")
+
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+class Admin(User):
+
+    def __init__(self, first_name, last_name, age, username, favorite_color):
+        super().__init__(first_name, last_name, age, username, favorite_color)
+
+        self.privileges = Privileges()
+
+class Privileges():
+
+    def __init__(self, privileges=[]):
+        self.privileges = privileges
+
+    def show_privileges(self):
+        print("\nPrivileges:")
+        if self.privileges:
+            for privilege in self.privileges:
+                print(f"- {privilege.title()}")
+        else:
+            print("This user has no privileges")
