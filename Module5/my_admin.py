c282390f4cf4c211e99658653a95c07659b9b004
ScarletPrescott <scarletelaineprescott@gmail.com>
2026-02-13 20:02:23 -0500

    my_admin.py

---
+++ Module5/my_admin.py
@@ -0,0 +1,16 @@
+#ScarletPrescott
+from admin import Admin
+
+user0 = Admin("Sarah", "Grech", 34, "sgrech", "green")
+user0.describe_user()
+
+user0_privileges = [
+    'can add post',
+    'can delete post',
+    'can ban user',
+    ]
+
+user0.privileges.privileges = user0_privileges
+
+print(f"Admin {user0.username} has privileges: ")
+user0.privileges.show_privileges()
