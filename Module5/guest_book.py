748ad3cc6e13348a2310d11c2d55e72a6ec045af
ScarletPrescott <scarletelaineprescott@gmail.com>
2026-02-13 20:01:09 -0500

    guest_book.py

---
+++ Module5/guest_book.py
@@ -0,0 +1,21 @@
+#ScarletPrescott
+from pathlib import Path
+
+path= Path('guest_book.txt')
+
+prompt = "\nEnter your name "
+prompt += "\nEnter 'quit' if you're the last guest: "
+
+guest_names = []
+while True:
+    name = input(prompt)
+    if name == 'quit':
+        break
+
+    print(f"{name}, we'll add you to the guest book.")
+    guest_names.append(name)
+
+file_string = ''
+for name in guest_names:
+    file_string += f"{name}\n"
+path.write_text(file_string)
